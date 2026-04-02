34d0426442e5737333ca7001fa23449725cfb94f
flow145 <268885866+flow145@users.noreply.github.com>
2026-04-02 16:10:51 +0500

    feat: add a websocket server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const http = require('http')
+const { WebSocketServer } = require('ws')
 
 const PORT = 3000
 
@@ -15,3 +16,32 @@ server.on('request', app)
 server.listen(PORT, () => {
   console.log(`Listening on ${PORT}`)
 })
+
+const wsServer = new WebSocketServer({ server })
+
+wsServer.on('connection', (ws) => {
+  const numClients = wsServer.clients.size
+  console.log(`Clients connected: ${numClients}`)
+  wsServer.broadcast(`Current visitors: ${numClients}`)
+
+  if (ws.readyState === ws.OPEN) ws.send('Welcome!')
+
+  ws.on('close', () => {
+    console.log('A client has disconnected')
+    wsServer.broadcast(`Current visitors: ${wsServer.clients.size}`)
+  })
+
+  ws.on('error', console.error);
+})
+
+/**
+ * Broadcast data to all connected clients
+ * @param {Object} data
+ * @void
+ */
+wsServer.broadcast = (data) => {
+  console.log('Broadcasting: ', data)
+  wsServer.clients.forEach((client) => {
+    client.send(data)
+  })
+}
